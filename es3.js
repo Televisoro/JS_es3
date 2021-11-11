9723e03545fa26633105d38a45cb1e474364d079
Galimberti Tommaso <galimberti.tommaso@osdbsesto>
2021-11-11 11:31:23 +0100

    Pisello

--- es3.js
+++ es3.js
@@ -1,55 +1,75 @@
-.$("#btn1").click(function(){
+for (let j = 0; j < localStorage.length; j++) {
+    $("#tab").append(localStorage.getItem(j));
+}
+
+var myStorage = window.localStorage;
+var i=0;
+
+$("button").click(function(){
     var input= $("#num1");
     var input2= $("#num2");
+    var sign = $(this).val();
     if(input.val() && input2.val()){
         var n1 = parseInt(input.val());
         var n2 = parseInt(input2.val());
-        var tot = n1+n2;
-        var string ="<tr><td>" + n1 + "</td><td>+</td><td>"+ n2 +"</td><td>"+ tot +"</td></tr>";
+        var tot;
+
+        switch (sign) {
+            case "+":
+                tot = n1 + n2;
+                break;
+            case "-":
+                tot = n1 - n2;
+                break;
+            case "*":
+                tot = n1 * n2;
+                break;
+            case "/":
+                tot = n1 / n2;
+                break;
+        };
+        var string ="<tr><td>" + n1 + "</td><td> "+ sign +" </td><td>"+ n2 +"</td><td>"+ tot +"</td></tr>";
+        localStorage.setItem(i, string); 
+        i=i+1;
         $("#tab").append(string);
         input.val("");
         input2.val("");
     } 
 });
 
-$("#btn2").click(function(){
-    var input= $("#num1");
-    var input2= $("#num2");
-    if(input.val() && input2.val()){
-        var n1 = parseInt(input.val());
-        var n2 = parseInt(input2.val());
-        var tot = n1-n2;
-        var string ="<tr><td>" + n1 + "</td><td>-</td><td>"+ n2 +"</td><td>"+ tot +"</td></tr>";
-        $("#tab").append(string);
-        input.val("");
-        input2.val("");
-    } 
-});
 
-$("#btn3").click(function(){
-    var input= $("#num1");
-    var input2= $("#num2");
-    if(input.val() && input2.val()){
-        var n1 = parseInt(input.val());
-        var n2 = parseInt(input2.val());
-        var tot = n1*n2;
-        var string ="<tr><td>" + n1 + "</td><td>*</td><td>"+ n2 +"</td><td>"+ tot +"</td></tr>";
-        $("#tab").append(string);
-        input.val("");
-        input2.val("");
-    } 
-});
+/*
+$("#btn1").click(function(){ operation("+")});
 
-$("#btn4").click(function(){
+
+
+var operation = function (sign) {
     var input= $("#num1");
     var input2= $("#num2");
     if(input.val() && input2.val()){
         var n1 = parseInt(input.val());
         var n2 = parseInt(input2.val());
-        var tot = n1/n2;
-        var string ="<tr><td>" + n1 + "</td><td>/</td><td>"+ n2 +"</td><td>"+ tot +"</td></tr>";
+        var tot;
+
+        switch (sign) {
+            case "+":
+                tot = n1 + n2;
+                break;
+            case "-":
+                tot = n1 - n2;
+                break;
+            case "*":
+                tot = n1 * n2;
+                break;
+            case "/":
+                tot = n1 / n2;
+                break;
+
+        };
+        var string ="<tr><td>" + n1 + "</td><td> "+ sign +" </td><td>"+ n2 +"</td><td>"+ tot +"</td></tr>";
         $("#tab").append(string);
         input.val("");
         input2.val("");
     } 
-});
+}
+*/
